fb11aa188e9cb00d1187c40cff25ea9261ff8f6c
Shubham Kukreti <shubhamkukreti204@gmail.com>
2022-07-03 22:03:31 +0530

    brew

--- third-week/pages/index.js
+++ third-week/pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
         <div className="w-full container mx-auto">
           <div className="w-full flex items-center justify-between">
             <a className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-              Loner<span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">Brew</span>
+              Loner<span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">$Brew</span>
             </a>
 
             <div className="flex w-1/2 justify-end content-center">
